600efdabad19440ad70eaba885ee57f020ef2657
David Hemphill <davidhemphill@knights.ucf.edu>
2019-09-18 18:35:39 -0400

    assignment.01

---
+++ assignment.01/Excercise_Four_ Return_ Multiples_5_between_1_and_n.R
@@ -0,0 +1,15 @@
+#Excercise Four Return Multiples 5 between 1 and n
+'?'(seq)
+'?'(unique)
+'?'(c)
+'?'(list)
+'?'(return)
+# ?Possibly begin with: n <- readline(prompt="Enter a number: ")
+n <- 100
+print(list(c(seq(1, n, 5))))
+#new try
+n <- 100
+print(list(unique(c(seq(1, n, 5)))))
+#new try
+n <- 100
+print(list(seq(1, n, 5)))
---
+++ assignment.01/Excercise_Six_avg_min_max_and_print.R
@@ -0,0 +1,36 @@
+#Excercise Six avg min max and print 
+#a
+a <- 1
+b <- 2
+c <- 3
+d <- 4
+e <- 5
+read.command <- function(a, b, c, d, e)
+  {
+  read(a,b c, d, e)
+}
+  read.command
+
+average.command <- function(x)
+{
+  average(read.command)
+}
+    average.command
+  
+#c  
+minimum.command <- function()
+{
+  min(read.command)
+}
+return(minimum.command)
+
+maximum.command <- function()
+{
+  max(read.command)
+}
+return(minimum.command)
+
+#d
+print("the average is" + average.command)
+print("the minimum is" + minimum.command)
+print("the maximum is" + maximum.command)
